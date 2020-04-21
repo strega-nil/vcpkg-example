cd056b29250ee73572ee912c0ddd057d2412ea0a
Nicole Mazzuca <nicole@strega-nil.co>
2020-04-21 00:26:30 -0700

    more complex example

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,20 @@
 #include <fmt/format.h>
+#include <cppitertools/itertools.hpp>
+
+int fib(int n) {
+	int a = 0, b = 1;
+
+	for ([[maybe_unused]] int i : iter::range(n)) {
+		auto tmp = b;
+		b = a + b;
+		a = tmp;
+	}
+
+	return a;
+}
 
 int main() {
-	fmt::print("Hello, {}!", "world");
+	for (int i : iter::range(10)) {
+		fmt::print("fib({}) = {}\n", i, fib(i));
+	}
 }
