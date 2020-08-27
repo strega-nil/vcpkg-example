6dddc26b9a9fa9d2f13ef63065910de3eb58c620
Nicole Mazzuca <mazzucan@outlook.com>
2020-08-27 14:53:31 -0700

    fix compile

--- src/main.cxx
+++ src/main.cxx
@@ -1,20 +1,6 @@
 #include <fmt/format.h>
-#include <cppitertools/itertools.hpp>
-
-int fib(int n) {
-	int a = 0, b = 1;
-
-	for ([[maybe_unused]] int i : iter::range(n)) {
-		auto tmp = b;
-		b = a + b;
-		a = tmp;
-	}
-
-	return a;
-}
+#include <zlib.h>
 
 int main() {
-	for (int i : iter::range(15)) {
-		fmt::print("fib({}) = {}\n", i, fib(i));
-	}
+	fmt::print("zlib version is {}, and the header version is {}\n", zlibVersion(), ZLIB_VERSION);
 }
